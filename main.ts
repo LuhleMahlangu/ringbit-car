08de93ae92d0787086af3f5fe912b293d9807a26
Luhle Mahlangu <125449200+LuhleMahlangu@users.noreply.github.com>
2026-05-21 14:55:55 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    RingbitCar.forward()
+})
+input.onButtonPressed(Button.B, function () {
+    RingbitCar.forward()
+})
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
